239008548b2a1fb2844be2758de2ac847d2bff3d
Mark Rosenberg <mrosenberg@prscholas.org>
2021-04-07 14:57:13 -0500

    Add project files.

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public delegate void CustomEventHandler(object sender, CustomEventArgs e);
+
+    public class CustomEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+        public CustomEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    public class Class1
+    {
+        int sec;
+        BackgroundWorker bw;
+        public event CustomEventHandler RaiseCustomEvent;
+
+        public void Start(int seconds)
+        {
+            sec = seconds;
+            bw = new BackgroundWorker
+            {
+                WorkerSupportsCancellation = true
+            };
+            bw.DoWork += bw_DoWork;
+            bw.RunWorkerCompleted += bw_RunWorkerCompleted;
+            bw.RunWorkerAsync();
+        }
+
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            OnRaiseCustomEvent(new CustomEventArgs("Timer Done"));
+        }
+
+        private void OnRaiseCustomEvent(CustomEventArgs eventArgs)
+        {
+            CustomEventHandler raiseEvent = RaiseCustomEvent;
+            if (raiseEvent != null)
+            {
+                raiseEvent(this, eventArgs);
+            }
+        }
+        public void Stop()
+        {
+            if (bw.IsBusy)
+            {
+                bw.CancelAsync();
+            }
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            DateTime nw = DateTime.Now;
+            while(nw.AddSeconds(sec) < DateTime.Now)
+            {
+                if (bw.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 c = new Class1();
+            c.Start(5);
+            c.RaiseCustomEvent += C_RaiseCustomEvent;
+        }
+
+        private void C_RaiseCustomEvent(object sender, CustomEventArgs e)
+        {
+            MessageBox.Show(e.Message,"", MessageBoxButtons.OK);
+        }
+    }
+}
